3575f5fd605f5b7dd0d7846753c8e18706c6ada3
Justin Moy <moy.justin94@gmail.com>
2022-10-10 14:26:26 -0500

    changes

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.use("/", mainRoutes);
 app.use("/post", postRoutes);
 app.use("/guest", mainRoutes);
 //Server Running
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log("Server is running, you better catch it!");
 });
