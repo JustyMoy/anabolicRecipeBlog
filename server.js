22552f06c1bb861b957708030e54311309ae20df
Justin Moy <moy.justin94@gmail.com>
2022-10-10 14:53:42 -0500

    changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const postRoutes = require("./routes/posts");
 
 
 //Use .env file in config folder
-require("dotenv").config({path: 'env'});
+require("dotenv").config({path: 'config.env'});
 
 // Passport config
 require("./config/passport")(passport);
